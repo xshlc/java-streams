ace079a025517eccb9ed108435b26c28505664b1
xshlc <heechuw@gmail.com>
2024-11-22 21:39:52 -0500

    map transform types

--- src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -12,11 +12,37 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.assertj.core.api.Assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TransformationsMapAndReduce {
 
     @Test
     void yourFirstTransformationWithMap() throws IOException {
         List<Person> people = MockData.getPeople();
+        // transform this list into a list of a different data type
+        // change Person type to PersonDTO
+        people.stream()
+                .map(person -> {
+                    return new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+                });
+
+        List<PersonDTO> peopleDTO = people.stream()
+                .map(person -> {
+                    PersonDTO personDTO = new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+                    return personDTO;
+                })
+                .collect(Collectors.toList());
+
+        //org.assertj.core.api.Assertions.assertThat(peopleDTO);
+        assertThat(people.size()).isEqualTo(peopleDTO.size());
+        // this lambda below can be replaced with a method reference
+//        peopleDTO.forEach(person -> {
+//            System.out.println(person);
+//        });
+        System.out.println("eeee");
+        peopleDTO.forEach(System.out::println); // here is the method reference
     }
 
     @Test
